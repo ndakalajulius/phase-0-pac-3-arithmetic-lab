131a7ae3a16b78a01cc8bbe853e48a060f32a513
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-12 10:32:43 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a,b){
+    return(a+b);
+}
+function subtract(a,b){
+    return(a-b);
+}
+function multiply(a,b){
+    return(a*b);
+}
+function divide(a,b){
+    return(a/b);
+}
+function increment(n){
+    return (++n);
+}
+function decrement(n){
+    return (--n);
+}
+const makeInt=(n)=>{
+    return parseInt(n,10);
+}
+const preserveDecimal=(n)=>{
+     return parseFloat(n);   
+}
